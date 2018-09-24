e8c633ce960c12ec2d092161f8f109ff39b57fcb
BuildTools <unconfigured@null.spigotmc.org>
2018-09-24 15:37:22 -0400

    Added run file

---
+++ Run.py
@@ -0,0 +1,11 @@
+#Imports
+import CatFacts
+from CatFacts import CatFactsBot
+
+def main():
+    catFactsBot = CatFactsBot()
+    catFactsBot.initialize()
+    catFactsBot.start()
+    return
+
+main()
